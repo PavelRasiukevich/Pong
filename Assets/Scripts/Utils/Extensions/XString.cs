df02edb5fbb517d336c0ed094db28c463fb72476
Pavel Rasiukevich <pavelrasiukevich@gmail.com>
2023-03-13 17:03:05 +0400

    string extension

--- Assets/Scripts/Utils/Extensions/XString.cs
+++ Assets/Scripts/Utils/Extensions/XString.cs
@@ -4,7 +4,7 @@ namespace Utils.Extensions
 {
     public static class XString
     {
-        public static string ColorString(this string str, Color color)
+        public static string PaintString(this string str, Color color)
         {
             return $"<color=#{color.ToHexStringWithoutAlpha()}>{str}</color>";
         }
